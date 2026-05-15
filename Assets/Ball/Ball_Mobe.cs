9efa1a06200657f0fbffcdd8ee605a086bf66fea
DESKTOP-7GMD8CR\GA2A <h1202515004@hiratagakuen.onmicrosoft.com>
2026-05-15 13:11:28 +0900

    コメント書いた

--- Assets/Ball/Ball_Mobe.cs
+++ Assets/Ball/Ball_Mobe.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.InputSystem.XR;
+using UnityEngine.UIElements;
 using static UnityEditor.PlayerSettings;
 using static UnityEngine.GraphicsBuffer;
 
@@ -8,85 +9,63 @@ public class NewMonoBehaviourScript : MonoBehaviour
     
     Rigidbody rb;
     //入力
-    float posX = 0.0f;
+    float Move = 0.0f;
     float posY = 0.0f;
-    float posZ = 0.0f;
+    float Rotation = 0.0f;
+    //ボールの速度
     float ball_speed = 0.8f;
-    //bool input_key_sensor = false;
-
-
+    float ball_Rotation_speed = 100.0f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        transform.eulerAngles = new Vector3(45, 0, 0);
+        //ボールを回転させる
+        //transform.eulerAngles = new Vector3(45, 0, 0);
+        //Rigidbody取得
         rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        //input_key_sensor = true;
-        //if (Input.GetKey(KeyCode.UpArrow))
-        //{
-        //    posZ += 1.0f;
-        //    input_key_sensor = false;
-        //}
-        //if (Input.GetKey(KeyCode.DownArrow))
-        //{
-        //    posZ -= 1.0f;
-        //}
-        //if (Input.GetKey(KeyCode.RightArrow))
-        //{
-        //    posX += 1.0f;
-        //}
-        //if (Input.GetKey(KeyCode.LeftArrow))
-        //{
-        //    posX -= 1.0f;
-        //}
-       
-        posX = Input.GetAxisRaw("Horizontal");
-        posZ = Input.GetAxisRaw("Vertical");
-        
-      
-
+        //ボールを動かすキーの取得
+        Move = Input.GetAxisRaw("Vertical"); 
+        Rotation = Input.GetAxisRaw("Horizontal");
     }
     private void FixedUpdate()
     {
-       
-            //移動
-            if (posX != 0 || posZ != 0)
+        Vector3 dir = transform.forward;
+        //移動
+        if (Move != 0 || Rotation != 0)
         {
-            rb.angularVelocity += new Vector3(posX * ball_speed, posY, posZ * ball_speed);
-            transform.Rotate(posX * ball_speed, posY, posZ * ball_speed);
+            rb.angularVelocity +=
+            new Vector3
+            (Rotation * ball_speed,
+            posY,
+            Move * ball_speed);
+
+            transform.Rotate(
+                Rotation * ball_Rotation_speed,
+                0,
+                0
+                );
         }
+        //ボールの動きが0.98を下回ると停止
         else
         {
             rb.angularVelocity *= 0.98f;
-            if (posX < 0.98 || posZ < 0.98)
+            if (Move < 0.98 || Rotation < 0.98)
             {
                 rb.angularVelocity *= 0.00f;
             }
         }
-        
 
+        //加速キーの取得と制御
         if (Input.GetKey(KeyCode.Space))
         {
-
-            // rb.angularVelocity = new Vector3(posX * 1000.0f, rb.angularVelocity.y, rb.angularVelocity.z);
             Debug.Log("スペースキー");
 
-            rb.angularVelocity += new Vector3(posX + 2.0f, posY+10.0f, posZ + 2.0f);
+            rb.angularVelocity += new Vector3(Move + 2.0f, posY + 10.0f, Rotation + 2.0f);
 
         }
-       
-       
-
-       
-
-        
-
-    
-
-        //rb.angularVelocity = new Vector3(rb.angularVelocity.x, rb.angularVelocity.y, posZ * 3.0f);
     }
 }
--- Assets/Camera/Ball_Chase.cs
+++ Assets/Camera/Ball_Chase.cs
@@ -2,25 +2,17 @@ using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+
+//前に移動するとカメラが前を向く　後ろを向くと後ろを向く　移動しないと前を向く
+//左右は移動するときにカメラも前を移しながら左右移動する
+//プレイヤーの左右の移動を方向転換にする
+
 public class Ball_Chase : MonoBehaviour
 {
-    float posX = 0.0f;
-    float posY = 0.0f;
-    float posZ = 0.0f;
-    float Chase_speed = 0.8f;
-    //public float topLimit = 0.0f;
-    //public float bottomLimit = 0.0f;
-    //public float leftLimit = 0.0f;
-    //public float rightLimit = 0.0f;
-    //public float frontLimit = 0.0f;
-    //public float backLimit = 0.0f;
-    //public Vector3 offset = new Vector3(3, 2, 0);
-
+   
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
-        // transform.eulerAngles = new Vector3(165, 90, 180);
         Screen.fullScreen = false;
         Screen.SetResolution(1280, 720, false);
         Cursor.lockState = CursorLockMode.Locked;
@@ -28,15 +20,14 @@ public class Ball_Chase : MonoBehaviour
     }
 
     // Update is called once per frame
-    
     void Update()
     {
-        //posX = Input.GetAxisRaw("Horizontal");
-        //posZ = Input.GetAxisRaw("Vertical");
+        //プレイヤーの位置取得
         GameObject player =
             GameObject.FindGameObjectWithTag("Player");
         if (player != null )
         {
+            //カメラの位置
             float x = player.transform.position.x + 3;
             float y = player.transform.position.y + 2;
             float z = player.transform.position.z;
@@ -44,13 +35,11 @@ public class Ball_Chase : MonoBehaviour
             Vector3 v3 = new Vector3(x, y, z);
             transform.localPosition = v3;
 
-            //transform.position = player.transform.position + offset;
-            //transform.LookAt(player.transform);
             //マウスカーソルで左右視点移動
             float mx = Input.GetAxis("Mouse X");//カーソルの横の移動量を取得
             float my = Input.GetAxis("Mouse Y");//カーソルの縦の移動量を取得
-            //new Vector3(posX * 1.0f, posY, posZ * 1.0f);
-            //float my = Input.GetAxis("Mouse Y");//マウスの横方向の移動量を取得
+
+            //マウスの左クリックを押している間視点をマウスで動かせる
             if (Input.GetMouseButton(0))
             {
                 Debug.Log("左クリックされた");
@@ -59,34 +48,14 @@ public class Ball_Chase : MonoBehaviour
                     transform.RotateAround(transform.position, Vector3.up, mx); // 回転軸はplayerオブジェクトのワールド座標Y軸
 
                 }
-                if (Mathf.Abs(my) > 0.001f)// Y方向に一定量移動していれば縦回転
-                {
-                    //transform.RotateAround(player.transform.position, Vector3.right, -my);
-                }
-               // transform.RotateAround(player.transform.position, Vector3.up, mx);
-               // transform.RotateAround(player.transform.position, transform.right, my);
-
-               // player.transform.rotation = Quaternion.Euler(0, transform.eulerAngles.y, 0);   // プレイヤーの向きを変更させる処理
-
             }
+            //マウスで動かした視点をマウスホイールでリセット
             if (Input.GetMouseButtonUp(2))
             {
                 transform.eulerAngles = new Vector3(165, 90, 180);
             }
-            //前に移動するとカメラが前を向く　後ろを向くと後ろを向く　移動しないと前を向く
-            //左右は移動するときにカメラも前を移しながら左右移動する
-            //プレイヤーの左右の移動を方向転換にする
-            posX = Input.GetAxisRaw("Horizontal");
-          　posZ = Input.GetAxisRaw("Vertical");
-            //移動
-            if (posX != 0 || posZ != 0)
-            {
-                //Component camera1 = camera;
-                //Vector3 pos = player.transform.position + camera1.transform.forward * Chase_speed;
-
-            }
+          
 
         }
-        
     }
 }
